552fbe4801eba3e85c66faf464647f228eec5cd4
prakash-4u <prakash96m@gmail.com>
2024-09-11 12:12:05 +0530

    Builder pattern Updated

--- LLD1/src/main/java/com/demo1/designPatterns/builderUpdated/Client.java
+++ LLD1/src/main/java/com/demo1/designPatterns/builderUpdated/Client.java
@@ -2,13 +2,27 @@ package com.demo1.designPatterns.builderUpdated;
 
 public class Client {
     public static void main(String[] args) {
-        Builder builder=new Builder();
-        builder.setName("Jegan");
-        builder.setAge(22);
-        builder.setBatch("Sep-24");
-        builder.setPsp(90.22);
-        builder.setGradYear(2021);
-        Student student=new Student(builder);
+//        Builder builder = Student.getBuilder();
+//        builder.setName("Jegan");
+//        builder.setAge(22);
+//        builder.setBatch("Sep-24");
+//        builder.setPsp(90.22);
+//        builder.setGradYear(2021);
+       // Student student=new Student(builder);
+      //  Student student=new Student(null);
+      //  Student student = builder.build();
+
+        Student student=Student.getBuilder()
+                .setAge(22)
+                   .setName("Jegan")
+                      .setBatch("Sep-24")
+                              .setPsp(90.33)
+                                      .setGradYear(2023).build();
+
+
+
+
+        System.out.println("debug");
     }
 
 }
--- LLD1/src/main/java/com/demo1/designPatterns/builderUpdated/Student.java
+++ LLD1/src/main/java/com/demo1/designPatterns/builderUpdated/Student.java
@@ -7,20 +7,81 @@ public class Student {
     private int gradYear;
     private String batch;
 
-    Student(Builder builder){
-      if(builder.getAge()<20){
-          throw new RuntimeException("Age must be above 20");
-      }
-      if(builder.getGradYear()>2022){
-          throw new RuntimeException("GradYear must be below 2022");
-      }
-      this.name = builder.getName();
-      this.age = builder.getAge();
-      this.psp = builder.getPsp();
-      this.gradYear = builder.getGradYear();
-      this.batch = builder.getBatch();
-      
+    private Student(Builder builder) {
+        if(builder.getAge()<20){
+            throw new RuntimeException("Age should be above 20");
+        }
+        if(builder.getGradYear()>2022){
+            throw new RuntimeException("GradYear should be below 2022");
+        }
+        this.name = builder.getName();
+        this.age = builder.getAge();
+        this.psp = builder.getPsp();
+        this.gradYear = builder.getGradYear();
+        this.batch = builder.getBatch();
+
+    }
+    public static Builder getBuilder(){
+        return new Builder();
     }
 
+    public static class Builder {
+        private String name;
+        private int age;
+        private double psp;
+        private int gradYear;
+        private String batch;
 
+        Builder(){
+
+        }
+        public Student build(){
+            return new Student(this);
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public Builder setName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        public Builder setAge(int age) {
+            this.age = age;
+            return this;
+        }
+
+        public double getPsp() {
+            return psp;
+        }
+
+        public Builder setPsp(double psp) {
+            this.psp = psp;
+            return this;
+        }
+
+        public int getGradYear() {
+            return gradYear;
+        }
+
+        public Builder setGradYear(int gradYear) {
+            this.gradYear = gradYear;
+            return this;
+        }
+
+        public String getBatch() {
+            return batch;
+        }
+
+        public Builder setBatch(String batch) {
+            this.batch = batch;
+            return this;
+        }
+    }
 }
